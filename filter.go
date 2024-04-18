f8cfcd655776eb72c254f15dc4c81dbafe268bd5
Ali <sr.ali.torabi@gmail.com>
2024-04-18 19:12:01 +0330

    debug filters column

--- filter.go
+++ filter.go
@@ -16,7 +16,7 @@ func (db *repo) Filter(gormQuery *gorm.DB, query dbcore.QueryModel) (q *gorm.DB,
 			s, op, vals := "", "", []any{}
 			for _, m := range filter.GetMatches() {
 				op = string(m.Op)
-				s += fmt.Sprintf("%s %s ? %s ", m.Key, m.Operator, m.Op)
+				s += fmt.Sprintf("`%s` %s ? %s ", m.Key, m.Operator, m.Op)
 				vals = append(vals, m.Value)
 			}
 
@@ -56,37 +56,37 @@ func (db *repo) dFilter(dbQuery *gorm.DB, query dbcore.QueryModel) (q *gorm.DB, 
 			field := strings.Split(v.Key, ".")
 			switch v.Operator {
 			case dfilter.OperatorIn:
-				q = q.Where(fmt.Sprintf("JSON_CONTAINS(%s->'$.%s', '[%s]')", field[0], field[1], strings.Join(v.Value.([]string), ",")))
+				q = q.Where(fmt.Sprintf("JSON_CONTAINS(`%s`->'$.%s', '[%s]')", field[0], field[1], strings.Join(v.Value.([]string), ",")))
 			case dfilter.OperatorNotIn:
 				//todo: implement not in
 			case dfilter.OperatorEq:
-				q = q.Where(fmt.Sprintf("%s->>'$.%s' = ?", field[0], field[1]), v.Value)
+				q = q.Where(fmt.Sprintf("`%s`->>'$.%s' = ?", field[0], field[1]), v.Value)
 			case dfilter.OperatorNot:
-				q = q.Where(fmt.Sprintf("%s->>'$.%s' != ?", field[0], field[1]), v.Value)
+				q = q.Where(fmt.Sprintf("`%s`->>'$.%s' != ?", field[0], field[1]), v.Value)
 			default:
-				q = q.Where(fmt.Sprintf("%s->>'$.%s'%s?", field[0], field[1], v.SQLOperator), v.Value)
+				q = q.Where(fmt.Sprintf("`%s`->>'$.%s'%s?", field[0], field[1], v.SQLOperator), v.Value)
 			}
 			continue
 		}
 		if t := query.GetDynamicFilterTable(); t != "" {
-			v.Key = fmt.Sprintf("%s.%s", t, v.Key)
+			v.Key = fmt.Sprintf("`%s`.`%s`", t, v.Key)
 		}
 		if items, ok := v.Value.([]string); ok {
 			filtered = true
 			if len(items) > 1 {
-				q = q.Where(fmt.Sprintf("%s IN (?)", v.Key), v.Value)
+				q = q.Where(fmt.Sprintf("`%s` IN (?)", v.Key), v.Value)
 				continue
 			}
 			if strings.Contains(items[0], ",") {
-				q = q.Where(fmt.Sprintf("%s IN (?)", v.Key), strings.Split(v.Value.([]string)[0], ","))
+				q = q.Where(fmt.Sprintf("`%s` IN (?)", v.Key), strings.Split(v.Value.([]string)[0], ","))
 				continue
 			}
 		}
 		if v.ValueType == dfilter.Null {
-			q = q.Where(fmt.Sprintf("%s IS NULL", v.Key))
+			q = q.Where(fmt.Sprintf("`%s` IS NULL", v.Key))
 			continue
 		}
-		q = q.Where(fmt.Sprintf("%s %s ?", v.Key, v.SQLOperator), v.Value)
+		q = q.Where(fmt.Sprintf("`%s` %s ?", v.Key, v.SQLOperator), v.Value)
 	}
 	return
 }
