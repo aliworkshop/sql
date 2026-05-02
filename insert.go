6e2d57e75448a4c256980342a1d03cd8fd2fe10a
Ali <sralitorabi@gmail.com>
2026-05-02 16:03:57 +0400

    refactor Insert function to use new errors package and improve error handling for duplicate entries

--- insert.go
+++ insert.go
@@ -2,22 +2,28 @@ package sql
 
 import (
 	"github.com/aliworkshop/dbcore"
-	"github.com/aliworkshop/error"
+	"github.com/aliworkshop/errors"
+	"github.com/jackc/pgx/v5/pgconn"
 )
 
-func (db *repo) Insert(query dbcore.QueryModel) (result interface{}, err error.ErrorModel) {
+func (db *repo) Insert(query dbcore.QueryModel) (result interface{}, err errors.ErrorModel) {
 	result = query.GetBody()
 	if result == nil {
-		err = errorHandler(error.New().
-			WithType(error.TypeValidation).
+		err = errorHandler(errors.New().
+			WithType(errors.TypeValidation).
 			WithId("InsertGetBodyNil").
 			WithDetail("nil body in insertion"))
 		return
 	}
 	dbc := db.GetGormDB(query).Create(result)
 	if dbc.Error != nil {
-		err = errorHandler(error.Internal(dbc.Error))
-		return
+		var e *pgconn.PgError
+		if errors.As(dbc.Error, &e) {
+			if e.Code == "23505" {
+				return nil, errors.Duplicate(dbc.Error)
+			}
+		}
+		return nil, errorHandler(errors.Internal(dbc.Error))
 	}
 	return
 }
